56094fead5b715aca827940853bfd1ab3c08342a
Avi Vahl <avi.vahl@wix.com>
2019-04-05 15:57:55 +0300

    Infra: reformat with semi-colons

--- src/create-temp-directory.ts
+++ src/create-temp-directory.ts
@@ -1,22 +1,22 @@
-import { tmpdir } from 'os'
-import { join } from 'path'
-import { promisify } from 'util'
-import { mkdtemp as mkdtempCb } from 'fs'
-import rimrafCb from 'rimraf'
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { promisify } from 'util';
+import { mkdtemp as mkdtempCb } from 'fs';
+import rimrafCb from 'rimraf';
 
-const rimraf = promisify(rimrafCb)
-const mkdtemp = promisify(mkdtempCb)
+const rimraf = promisify(rimrafCb);
+const mkdtemp = promisify(mkdtempCb);
 
 export interface ITempDirectory {
     /**
      * Absolute path to created directory.
      */
-    path: string,
+    path: string;
 
     /**
      * Remove the directory and all its contents.
      */
-    remove(): Promise<void>
+    remove(): Promise<void>;
 }
 
 /**
@@ -26,8 +26,8 @@ export interface ITempDirectory {
  * @returns an absolute `path` and a `remove()` function.
  */
 export async function createTempDirectory(prefix = 'temp-'): Promise<ITempDirectory> {
-    const path = await mkdtemp(join(tmpdir(), prefix))
-    const remove = () => rimraf(path)
+    const path = await mkdtemp(join(tmpdir(), prefix));
+    const remove = () => rimraf(path);
 
-    return { path, remove }
+    return { path, remove };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { createTempDirectory, ITempDirectory } from './create-temp-directory'
+export { createTempDirectory, ITempDirectory } from './create-temp-directory';
