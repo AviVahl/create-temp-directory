e2968c4bc87200039194334b33b0fb9b54aee079
Avi Vahl <avi.vahl@wix.com>
2019-10-14 19:23:02 +0300

    feat: implement sync version

--- src/create-temp-directory.ts
+++ src/create-temp-directory.ts
@@ -1,7 +1,7 @@
 import { tmpdir } from 'os';
 import { join } from 'path';
 import { promisify } from 'util';
-import { mkdtemp as mkdtempCb } from 'fs';
+import { mkdtemp as mkdtempCb, mkdtempSync } from 'fs';
 import rimrafCb from 'rimraf';
 
 const rimraf = promisify(rimrafCb);
@@ -28,6 +28,31 @@ export interface ITempDirectory {
 export async function createTempDirectory(prefix = 'temp-'): Promise<ITempDirectory> {
     const path = await mkdtemp(join(tmpdir(), prefix));
     const remove = () => rimraf(path);
+
+    return { path, remove };
+}
+
+export interface ITempDirectorySync {
+    /**
+     * Absolute path to created directory.
+     */
+    path: string;
+
+    /**
+     * Remove the directory and all its contents.
+     */
+    remove(): void;
+}
+
+/**
+ * Create an empty, unique directory in the current OS's temp directory.
+ *
+ * @param prefix optional prefix to add to the random name. (default: "temp-")
+ * @returns an absolute `path` and a `remove()` function.
+ */
+export function createTempDirectorySync(prefix = 'temp-'): ITempDirectorySync {
+    const path = mkdtempSync(join(tmpdir(), prefix));
+    const remove = () => rimrafCb.sync(path);
 
     return { path, remove };
 }
